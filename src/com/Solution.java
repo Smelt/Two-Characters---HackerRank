b73c750e1011137d700d1f116c8f7ca9d1995f8f
Smelt <smelting103@gmail.com>
2017-01-28 16:59:49 -0500

    First

---
+++ src/com/Solution.java
@@ -0,0 +1,89 @@
+package com;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Solution {
+
+	/*
+	 * Two Characters
+	 */
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+        int len = in.nextInt();
+        String s = in.next();
+        char[] schar = s.toCharArray();
+   
+        //we determine all of the unique characters
+        HashSet<Character> unique = new HashSet<Character>();
+        for(int i = 0; i < schar.length; i++){
+            unique.add(schar[i]);
+        }
+      
+        //create a HashSet of Every Combination
+        String alphabet = "";
+        for(char letter: unique){
+          alphabet = alphabet + letter;
+        }
+        HashSet<String> combos = pairs(alphabet.toCharArray());
+        
+        //create a list of all the types the modified Strings
+        ArrayList<String> modStrings = new ArrayList<String>();
+        for(String str: combos){
+           modStrings.add(createString(str,s));
+        }
+      
+        int max = 0;
+        int value = 0;
+        for(String str: modStrings){
+            value = checkRepeat(str.toCharArray());
+            if(value > max){
+                max = value;
+            }
+        }
+        
+        System.out.println(max);
+    }
+  
+  private static String createString(String combo, String input){
+      char[] ab = combo.toCharArray();
+      char a = ab[0];
+      char b = ab[1];
+      char[] inputArr = input.toCharArray();
+      String str = "";
+    
+      for(int i = 0; i < inputArr.length; i++){
+          if(inputArr[i] == a || inputArr[i] == b){
+              str = str + inputArr[i];
+          }
+      }
+      return str;
+
+  }
+  
+  private static HashSet<String> pairs(char[] alpha){
+		HashSet<String> combos = new HashSet<String>();
+		for(int i = 0; i < alpha.length; i++){
+			for(int x = i + 1; x < alpha.length; x++){
+				String element = alpha[i] + "" + alpha[x];
+				combos.add(element);
+			}
+		}
+		return combos;
+	}
+  
+    private static int checkRepeat(char[] input){
+    
+        for(int i = 1; i < input.length; i++){
+            char a = input[i - 1];
+            char b = input[i];
+            if(a == b){
+                return -1;
+            }
+        }
+      
+        return input.length;
+    }
+}
